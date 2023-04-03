f4a2d01b51eba56b7c07f4f37f8f6f922867f7af
pharezende <phamorimrezende@gmail.com>
2023-04-03 08:52:51 +0000

    fix: 104 -> 200

--- set_traffic_rules.sh
+++ set_traffic_rules.sh
@@ -3,7 +3,7 @@
 docker exec -it upf iptables -F
 docker exec -it upf iptables-legacy -A FORWARD -s 192.168.100.0/24 -d 100.0.0.0/24 -j ACCEPT
 docker exec -it upf iptables-legacy -A FORWARD -s 192.168.100.0/24 -j DROP
-docker exec -it upf iptables-legacy -A FORWARD -s 192.168.104.0/24 -j DROP
+docker exec -it upf iptables-legacy -A FORWARD -s 192.168.200.0/24 -j DROP
 
 docker exec -it upf2 iptables -F
 docker exec -it upf2 iptables-legacy -A FORWARD -s 192.168.200.0/24 -d 10.0.1.0/24 -j ACCEPT
